8b3818d5756888bd09f4806ebd468b0f62596992
Joshua Kristanto <joshuakristanto68@gmail.com>
2020-02-24 00:39:28 -0800

    Fixed Scrolling

--- NavigationBar/NavigationBar/ThirdViewController.swift
+++ NavigationBar/NavigationBar/ThirdViewController.swift
@@ -19,15 +19,46 @@ class ThirdViewController: UIViewController {
                return prices
        
        }
-    @IBOutlet weak var imageView: UIStackView!
+//    @IBOutlet var scrollView: UIScrollView!
+//    @IBOutlet weak var imageView: UIStackView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        imageView.heightAnchor.constraint(equalToConstant: view.frame.height - 100).isActive = true
-         imageView.widthAnchor.constraint(equalToConstant: view.frame.height - 40).isActive = true
-        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        imageView.heightAnchor.constraint(equalToConstant: scrollView.frame.height - 100).isActive = true
+//         imageView.widthAnchor.constraint(equalToConstant: scrollView.frame.height - 40).isActive = true
+//        imageView.centerYAnchor.constraint(equalTo: scrollView.centerYAnchor).isActive = true
+//        imageView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
+        
+        let scrollView = UIScrollView()
+           self.view.addSubview(scrollView)
+
+           scrollView.translatesAutoresizingMaskIntoConstraints = false
+           NSLayoutConstraint.activate([
+             scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+             scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+             scrollView.topAnchor.constraint(equalTo: self.view.topAnchor),
+             scrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+           ])
+        
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.spacing = 20
+        stackView.distribution = .fill
+        scrollView.addSubview(stackView)
+
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+          // Attaching the content's edges to the scroll view's edges
+          stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+          stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+          stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+          stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+
+          // Satisfying size constraints
+          stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+        ])
 //        imageView.translatesAutoresizingMaskIntoConstraints = false
 //        imageView.distribution = .fillEqually
 //        imageView.axis = .vertical
@@ -39,7 +70,7 @@ class ThirdViewController: UIViewController {
 
         do {
           let contents = try fileManager.contentsOfDirectory(at: assetURL, includingPropertiesForKeys: [URLResourceKey.nameKey, URLResourceKey.isDirectoryKey], options: .skipsHiddenFiles)
-
+ var a = 0
           for item in contents
           {
               print(item.lastPathComponent)
@@ -48,8 +79,16 @@ class ThirdViewController: UIViewController {
             
             let one = UIImageView(image:  image)
 //            let one = UIImageView(image: (id)initWithContentsOfFile:(NSString *)item.lastPathComponent))
-            imageView.addArrangedSubview(one)
-          }
+            if(a < 101)
+            {
+            stackView.addArrangedSubview(one)
+             one.translatesAutoresizingMaskIntoConstraints = false
+             one.heightAnchor.constraint(equalToConstant: 200).isActive = true
+                one.clipsToBounds = true
+                one.contentMode = .scaleAspectFit
+                a = a + 1
+            }
+            }
         }
         catch let error as NSError {
           print(error)
