2f304a261af0d7ec3feefce9fffc124b7e6df1f8
Joshua Kristanto <joshuakristanto68@gmail.com>
2020-03-20 03:42:23 -0700

    Added AR Functionality

--- NavigationBar/NavigationBar/SecondViewController.swift
+++ NavigationBar/NavigationBar/SecondViewController.swift
@@ -7,14 +7,62 @@
 //
 
 import UIKit
+import ARKit
+import RealityKit
 
 class SecondViewController: UIViewController {
+    
+    var arView = ARView(frame: .zero)
+    var anchor:ARTEST.Scene!
 
+    override func loadView(){
+        super.loadView()
+        view.addSubview(arView)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        arView.frame = view.frame
+//        ARTEST.Scene.look(<#T##self: ARTEST.Scene##ARTEST.Scene#>)
+        anchor = try! ARTEST.loadScene()
+//        scene2 = try! ARTEST.
+//        anchor.generateCollisionShapes(recursive: true)
+        print("HElloWorld")
+        
+        self.arView.scene.anchors.append(anchor)
+//
+//        anchor.notifications.starter.post()
+//        ARTEST.loadSceneAsync { (result, error) in
+//            guard error == nil else {
+//                return
+//            }
+//
+//            if let ARTEST = result {
+//                self.arView.scene.addAnchor(ARTEST)
+//            }
+//
+//        }
+//        ARTEST.loadSceneAsync{(Result,<ARTEST.Scene, Error>) in
+//            guard error == nil else{
+//                return
+//            }
+//            if let spinnerScene = result{
+//                self.arView.scene.addAnchor(spinnerScene)
+//            }
+//
+//
+//    }
+    
     }
-
+    
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        // 6: create an ARWorldTrackingConfiguration
+//        let configuration = ARWorldTrackingConfiguration()
+//        // 7: run the configuration
+//        arView.session.run(configuration)
+//    }
+    
+   
 
 }
 
